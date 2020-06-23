ea8cca558597c06bc81c224143c490a372a7469e
Zahra <zahrash.outlook.com>
2020-06-24 02:10:19 +0300

    Add project files.

---
+++ E-Library/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_Library.Startup))]
+namespace E_Library
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
